93bf91c03111456c357e87853877a98eb1ae6712
calebsheridan <calebsheridan@users.noreply.github.com>
2024-04-12 12:59:42 +0200

    Register mantine nofitications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,26 +5,34 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '@mantine/core/styles.css';
 import { createTheme, MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 
 const queryClient = new QueryClient();
 
 const theme = createTheme({
-    cursorType: 'pointer',
-  });
+  cursorType: 'pointer',
+});
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <MantineProvider theme={theme}>
-      <Head>
-        <title>TX Explain</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-      </Head>
+        <Head>
+          <title>TX Explain</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+          />
+        </Head>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
+        <Notifications
+          position="top-right"
+          pos="absolute"
+          right={0}
+          top={0}
+          p={16}
+        />
       </MantineProvider>
     </QueryClientProvider>
   );
