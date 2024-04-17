b77cca8d8f4822c1609b9f8183cdea2b7f37f4ce
Dorian Janezic <dtj5179@nyu.edu>
2024-04-17 13:33:03 +0200

    navigate through transactions in a given block

--- components/TxDetails.tsx
+++ components/TxDetails.tsx
@@ -1,65 +1,97 @@
-import { Box, Card, Title, Text } from "@mantine/core"
+import React, { useState } from 'react';
+import { Box, Card, Title, Text, Button } from "@mantine/core";
 import { formatUnits } from "viem";
 import { useTransaction, useBlock } from 'wagmi';
 
-const TxDetails = (
-    { transactionHash
-    }: {
-        transactionHash: `0x${string}` | undefined;
-    }) => {
-    const { data: transactionReceipt, isLoading: isTransactionReceiptLoading } = useTransaction({
+// Function to render a row of transaction details
+const txDetailRow = (label: string, value: any) => {
+    return (
+        <Box display="flex">
+            <Text style={{ whiteSpace: "nowrap" }} w="25%" size="xs" color="dimmed">{label}</Text>
+            <Text w="75%" size="xs">{value ?? 'N/A'}</Text>
+        </Box>
+    );
+}
+
+const TxDetails = ({
+    transactionHash
+}: {
+    transactionHash: `0x${string}` | undefined;
+}) => {
+    // Fetch transaction details based on the provided hash
+    const { data: transactionReceipt, isLoading: isTransactionReceiptLoading, status } = useTransaction({
         hash: transactionHash,
     });
 
+    // Fetch block details related to the transaction
     const block = useBlock({
-        blockHash: transactionReceipt?.blockHash
-    })
+        blockHash: transactionReceipt?.blockHash,
+        includeTransactions: true
+    });
 
-    const gasUsed = transactionReceipt?.gas ? transactionReceipt.gas.toString() : undefined;
-    const baseFee = block.data?.baseFeePerGas ? `${formatUnits(block.data.baseFeePerGas, 9)} gwei` : undefined;
-    const gasPrice = transactionReceipt?.gasPrice ? `${formatUnits(transactionReceipt.gasPrice, 9)} gwei` : undefined;
-    const maxFee = transactionReceipt?.maxFeePerGas ? `${formatUnits(transactionReceipt.maxFeePerGas, 9)} gwei` : undefined;
-    const maxPriorityFee = transactionReceipt?.maxPriorityFeePerGas ? `${formatUnits(transactionReceipt.maxPriorityFeePerGas, 9)} gwei` : undefined;
-    const gasUsedInEth = transactionReceipt?.gas && transactionReceipt?.gasPrice ? `${formatUnits(transactionReceipt.gas * transactionReceipt.gasPrice, 9)} ETH` : undefined;
+    // State to keep track of the current transaction index
+    const [currentTxIndex, setCurrentTxIndex] = useState<number>(0);
 
-    const txDetailRow = (label: string, value: string | undefined | `0x${string}` | null | number) => {
-        return (
-            <Box display="flex">
-                <Text style={{ whiteSpace: "nowrap" }} w="25%" size="xs" c="dimmed">{label}</Text>
-                {value !== undefined && <Text w="75%" size="xs">{value}</Text>}
-            </Box>
-        )
-    }
+    // Function to navigate to the previous or next transaction in a block
+    const handleNavigateTx = (direction: 'next' | 'prev') => {
+        setCurrentTxIndex((prevIndex) => {
+            const transactionsLength = block.data?.transactions?.length ?? 0;
+            if (direction === 'next') {
+                return prevIndex === transactionsLength - 1 ? 0 : prevIndex + 1;
+            } else {
+                return prevIndex === 0 ? transactionsLength - 1 : prevIndex - 1;
+            }
+        });
+    };
+
+    if (isTransactionReceiptLoading) return <Text>Loading transaction details...</Text>;
+    if (!transactionReceipt) return <Text>Transaction hash invalid or transaction not found.</Text>;
+
+    const currentTx = block.data?.transactions[currentTxIndex];
+    console.log("CURRENT TX:", currentTx);
+
+    // Format current transaction details
+    const value: string | undefined = currentTx?.value ? `${formatUnits(currentTx.value, 18)} ETH` : undefined;
+    const gasUsed: string | undefined = currentTx?.gas ? currentTx.gas.toString() : undefined;
+    const baseFee: string | undefined = block.data?.baseFeePerGas ? `${formatUnits(block.data.baseFeePerGas, 9)} gwei` : undefined;
+    const gasPrice: string | undefined = currentTx?.gasPrice ? `${formatUnits(currentTx.gasPrice, 9)} gwei` : undefined;
+    const maxFee: string | undefined = currentTx?.maxFeePerGas ? `${formatUnits(currentTx.maxFeePerGas, 9)} gwei` : undefined;
+    const maxPriorityFee: string | undefined = currentTx?.maxPriorityFeePerGas ? `${formatUnits(currentTx.maxPriorityFeePerGas, 9)} gwei` : undefined;
+    const gasUsedInEth: string | undefined = currentTx?.gas && currentTx?.gasPrice ? `${formatUnits(currentTx.gas * currentTx.gasPrice, 18)} ETH` : undefined;
+    const txIndex: number | undefined = currentTx?.transactionIndex;
 
     return (
         <Box mb="xl">
-            <Title order={2} mb="md">
-                Transaction Details
-            </Title>
+            <Title order={2} mb="md">Transaction Details</Title>
             <Card shadow="sm" p="lg" radius="md" withBorder mb="xl">
-                {txDetailRow("Chain ID:", transactionReceipt?.chainId)}
-                {txDetailRow("Block Number:", transactionReceipt?.blockNumber.toString())}
-                {txDetailRow("Tx Hash:", transactionReceipt?.hash)}
-                {txDetailRow("Position In Block:", transactionReceipt?.transactionIndex)}
-                {/* {txDetailRow("Block Hash:", transactionReceipt?.blockHash)} */}
+                {txDetailRow("Status:", status)}
+                {txDetailRow("Chain ID:", currentTx?.chainId)}
+                {txDetailRow("Block Number:", currentTx?.blockNumber.toString())}
+                {txDetailRow("Tx Hash:", currentTx?.hash)}
+                {txDetailRow("Position In Block:", txIndex)}
                 <Box mb="xs"></Box>
-                {txDetailRow("From:", transactionReceipt?.from)}
-                {txDetailRow("To:", transactionReceipt?.to)}
+                {txDetailRow("From:", currentTx?.from)}
+                {txDetailRow("To:", currentTx?.to)}
+                {txDetailRow("Value:", value === undefined ? 0 + " ETH" : value)}
                 <Box mb="xs"></Box>
-                {txDetailRow("Nonce:", transactionReceipt?.nonce)}
-                {txDetailRow("Input Data:", transactionReceipt?.input)}
+                {txDetailRow("Nonce:", currentTx?.nonce)}
+                {txDetailRow("Input Data:", currentTx?.input)}
                 <Box mb="xs"></Box>
                 {txDetailRow("Gas Used:", gasUsed)}
-                {txDetailRow("TypeHex:", transactionReceipt?.typeHex)}
-                {txDetailRow("Type:", transactionReceipt?.type)}
+                {txDetailRow("TypeHex:", currentTx?.typeHex)}
+                {txDetailRow("Type:", currentTx?.type)}
                 {txDetailRow("Transaction Fee:", gasUsedInEth)}
                 {txDetailRow("Gas Price:", gasPrice)}
                 {txDetailRow("Base:", baseFee)}
                 {txDetailRow("Max:", maxFee)}
                 {txDetailRow("Max Priority:", maxPriorityFee)}
             </Card>
-        </Box >
+            <Box display="flex">
+                <Button onClick={() => handleNavigateTx('prev')} style={{ marginRight: '10px' }}>Previous Transaction</Button>
+                <Button onClick={() => handleNavigateTx('next')}>Next Transaction</Button>
+            </Box>
+        </Box>
     );
 };
 
-export default TxDetails
+export default TxDetails;
